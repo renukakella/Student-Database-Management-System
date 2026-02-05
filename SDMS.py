8b1e5cc0d8dd4db5306b0893fa7980d319d1bc69
renukakella <141899466+renukakella@users.noreply.github.com>
2026-02-05 15:42:28 +0530

    SDMS Using Basics

--- SDMS.py
+++ SDMS.py
@@ -1,27 +1,3 @@
-'''Student Database Management System (Python)
-
-This is a beginner-level Python mini project.
-Features:
-- Add, Update, Delete students
-- View all students
-- Find topper
-
-Concepts used:
-- Python dictionaries
-- Loops
-- Conditional statements
-- Input validation
-
-This project was built while learning Python fundamentals.
-
-Initial student management system using Python basics
-Future Improvements:
-- Convert code to functions
-- Add file handling
-- Add search & sort features
-
-'''
-
 students = {}  # key = student_id, value = {name, age, marks}
 
 while True:
@@ -133,3 +109,4 @@ while True:
         # invalid
         print("-"*10, "Invalid Choice", "-"*10)
         
+
